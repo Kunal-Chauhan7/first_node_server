68b893bb38a3b9ccb70767e51849fb5db29a0e8c
Kunal Chauhan <kunalchauhanlion@gmail.com>
2023-03-04 01:41:14 +0530

    day 3 done

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 const http = require("http");
 
 const port = 8107
+const todo = ["study Web-Dev" , "Study python" , "Study Java"]
 
-http.createServer((request , responce)=>{
-    responce.writeHead(200,{"Content-Type":"text/html"});
-    responce.write(`<h1> hello this message genrated by server aka Kunal Chauhan</h1>`);
+http.createServer((request, responce)=>{
+    const{method,url} = request;
+    console.log(method,url)
+    if(url === "/todos"){
+        if(method === "GET"){
+            responce.writeHead(200 , {"Content-Type" : "text/html"});
+            responce.write(`<h2>${todo.toString()}</h2>`);
+        } else if(method === "POST"){
+            let body = "";
+            request.on('error',(err) =>{
+                console.log(err)
+            }).on('data',(chunk) => {
+                body += chunk;
+
+            }).on('end',() => {
+                body = JSON.parse(body);
+                console.log(body)
+            })
+        }
+        else{
+            responce.writeHead(501);
+        }
+    } else {
+        responce.writeHead(404);
+    }
     responce.end();
 })
 .listen(port,()=>{
